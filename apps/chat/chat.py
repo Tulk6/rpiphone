5af3a58051b0dfd1e9c8e7a1fcf71198e0751f81
itisacrouton <itisacrouton@proton.me>
2023-12-13 22:48:05 +1100

    Got basic sending/receiving working for one active chat

--- apps/chat/chat.py
+++ apps/chat/chat.py
@@ -1,5 +1,6 @@
 import pyray as pr
 import apps.chat.xmppManager as xmppManager
+import menuUtils as mu
 import tomllib, yaml
 
 class ConversationObject:
@@ -26,6 +27,7 @@ class ConversationObject:
 class App:
     def __init__(self, mstr):
         self.mstr = mstr
+        mu.associateMaster(mstr)
         self.loadSettings()
         self.loadAddressBook() #address book is local to the phone !
                                 #contacts is local to XMPP server
@@ -36,9 +38,7 @@ class App:
         
         self.xmppManager.openClient()
         self.xmppManager.sendUnavailablePresence('jamestulk@jabbers.one')#, 'Hello!', show='dnd')
-        self.xmppManager.registerHandlers(self.handleMessage, self.receiveMessage)
-
-        self.xmppManager.sendMessage('Is it working?? !', 'jamestulk@jabbers.one')
+        self.xmppManager.registerHandlers(self.handlePresence, self.handleMessage)
 
         self.state = 'Conversations' #app should start showing the lsit of contacts
         self.conversationIndex = 0
@@ -55,14 +55,27 @@ class App:
         self.background = pr.load_texture('apps/chat/background.png')
         self.pixelFontHeading = pr.load_font_ex('ui/fonts/invasion2000.regular.ttf', 24, None, 0)
 
-    def handleMessage(self, con, event):
+        self.textLogger = mu.TextLogger()
+        self.textLogger.setKeyboard(False)
+
+    def handlePresence(self, con, event):
         self.xmppManager.updateContacts()
         if self.chatJid is not None:
             self.chatContact = self.xmppManager.contacts[self.chatJid]
-        print(con)
-        print(event)
         #print(event.getType())
         #print(event.getBody())
+        
+    def handleMessage(self, con, event):
+        #print('con' + str(type(con)))
+        #print('event' + str(type(event)))
+        #print(event.getType())
+        #print('DATA HELLO is antQ!@ELOORLKRWLKJELJ')
+        #print(event.getData())
+        if event.getType() == 'chat' and event.getBody() is not None:
+            #get from returns wack address with resouce, get stripped is usual node@domain.com
+            self.chatMessages.append((self.xmppManager.roster.getName(event.getFrom().getStripped()), event.getBody()))
+        elif event.getType() == 'error':
+            print('---> ERROR: ', event.getBody())
 
     def determineDisplayShow(self, contact):
         if contact['status'] is None and contact['show'] is None: #an offline presence is None status and Show (available is None show)
@@ -94,23 +107,17 @@ class App:
     def loadAddressBook(self):
         with open('user/address_book/address_book.yaml', 'r') as file:
             self.addressBook = yaml.safe_load(file)
-    
-    def receiveMessage(self, con, event):
-        print('con', type(con))
-        print('event', type(event))
-        print(event.getType())
-        if event.getType() == 'chat' and event.getBody() is not None:
-            event.get
-            #get from returns wack address with resouce, get stripped is usual node@domain.com
-            self.chatMessages.append((self.xmppManager.roster.getName(event.getFrom().getStripped()), event.getBody()))
-        elif event.getType() == 'error':
-            print('---> ERROR: ', event.getBody())
 
     def startChat(self, jid):
         self.state = 'Chat'
         self.chatJid = jid
         self.chatContact = self.xmppManager.contacts[jid]
         self.chatMessages = []
+        self.textLogger.start()
+
+    def sendMessageInCurrentChat(self, msg):
+        self.chatMessages.append((self.xmppManager.username, msg))
+        self.xmppManager.sendMessage(msg, self.chatJid)
 
     def update(self):
         self.xmppManager.process()
@@ -122,6 +129,11 @@ class App:
 
             if pr.is_key_pressed(pr.KEY_ENTER):
                 self.startChat(self.xmppManager.contactJids[self.conversationIndex])
+
+        elif self.state == 'Chat':
+            if pr.is_key_pressed(pr.KEY_ENTER):
+                self.sendMessageInCurrentChat(self.textLogger.text)
+                self.textLogger.reset()
 
         #print(self.xmppManager.contacts.getStatus('jamestulk@jabbers.one'))
 
@@ -139,8 +151,11 @@ class App:
             pr.draw_text_ex(self.mstr.uiFont, f'Currently {self.determineDisplayShow(self.chatContact)}',
                             (10, 46), 16, 1, self.mstr.WHITE)
             
+            pr.draw_rectangle_rounded_lines(pr.Rectangle(10,270, 220, 40), 0.4, 2, 1, self.mstr.WHITE)
+            pr.draw_text_ex(self.mstr.uiFont, self.textLogger.getText(), (10, 270), 16, 1, self.mstr.WHITE)
+
             for i, (sender, body) in enumerate(reversed(self.chatMessages)):
-                pr.draw_text_ex(self.mstr.uiFont, f'{sender}: {body}', (10, 280-(10*i)), 16, 1, self.mstr.WHITE)
+                pr.draw_text_ex(self.mstr.uiFont, f'{sender}: {body}', (10, 250-(12*i)), 16, 1, self.mstr.WHITE)
 
 
     def close(self):
